61dee89eb259aa0a6232829ac233f66f12b72cd4
lizOcHkA9 <151072073+lizOcHkA9@users.noreply.github.com>
2024-01-01 16:04:17 +0300

    Update raycasing.py

--- raycasing.py
+++ raycasing.py
@@ -41,3 +41,23 @@ def ray_casting(screen, pos, angle):
         cur_angle += DELTA_ANGLE
         angle %= 360
         cur_angle %= 6.28
+
+
+def ray_casting_bot(screen, pos, angle, delta):
+    cur_angle = math.radians(angle)
+    x1, y1 = pos
+    for i in range(4):
+        sin_a = math.sin(cur_angle)
+        cos_a = math.cos(cur_angle)
+        for j in range(MAX_DEPTH):
+            x = x1 + j * cos_a
+            y = y1 + j * sin_a
+            pygame.draw.line(screen, 'gray', bot_coord, (x, y))
+            try:
+                wall = map_world[int(x // minimap_size)][int(y // minimap_size)]
+                if wall:
+                    break
+            except IndexError:
+                continue
+
+        cur_angle += math.radians(delta)
