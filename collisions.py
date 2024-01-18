15c3f7969e91911d241e5b7450a16e2cc91b4476
vailyevKA <151026226+vailyevKA@users.noreply.github.com>
2024-01-18 19:22:26 +0300

    Add files via upload

---
+++ collisions.py
@@ -0,0 +1,17 @@
+import math
+from settings import *
+
+
+def can_go(player_x, player_y, angle, is_ahead, speed):
+    if is_ahead:
+        next_x = player_x + (minimap_size / 30) * (math.cos(math.radians(angle))) * speed
+        next_y = player_y + (minimap_size / 30) * (math.sin(math.radians(angle))) * speed
+    else:
+        next_x = player_x - (minimap_size / 30) * (math.cos(math.radians(angle))) * speed
+        next_y = player_y - (minimap_size / 30) * (math.sin(math.radians(angle))) * speed
+
+    if map_world[int(next_x // minimap_size)][int(next_y // minimap_size)]:
+        return False
+    if map_world[int((next_x + 3) // minimap_size)][int((next_y + 3) // minimap_size)] == 2:
+        map_world[int((next_x + 3) // minimap_size)][int((next_y + 3) // minimap_size)] = 1
+    return True
